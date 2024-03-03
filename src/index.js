439c1b9621f6eee4642c4b63af49b005d49102ed
Bibaswan Chakma <bibaswanch1769@gmail.com>
2024-03-04 00:27:06 +0530

    mongoDB configured and connected

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import connectDB from './db/index.js';
+import { app } from './app.js';
+
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3000, ()=>{
+        console.log(`server is running ar port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed! ", err);
+})
+; 
