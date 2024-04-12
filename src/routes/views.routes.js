0dbe85d51eb9fde0eab61ec5ddad8225b10d310f
Bibaswan Chakma <bibaswanch1769@gmail.com>
2024-04-12 23:39:27 +0530

    Appointment request and functionality added

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const teacherModel = require('../models/teacher.model')
 const appointment = require('../models/appointment.model')
 const isAuth = require('../controllers/user.authentication')
+
+
 //views routes
 router.get('/', (req, res,next)=>{
     res.render('index');
@@ -27,15 +29,15 @@ router.get('/teachersignup', (req, res)=>{
 router.get('/studentdashboard',isAuth, async (req,res)=>{
     const studentData = req.session.user;
     const teachers = await teacherModel.find();
-    const approvedAppointments = await appointment.find({status: 'approved'})
+    const approvedAppointments = await appointment.find({ $and: [{ studentID: studentData.id }, { status: 'approved' }] })
     res.render('studentdashboard', {student: studentData, teachers : teachers, approvedappointment: approvedAppointments});
 
 })
 
 router.get('/teacherdashboard',isAuth, async (req,res)=>{
-    const pendingappointments = await appointment.find({status : 'pending'})
-    const approvedAppointments = await appointment.find({status: 'approved'})
     const teacherData = req.session.user;
+    const pendingappointments = await appointment.find({ $and: [{ teacherID: teacherData.id}, { status: 'pending' }]})
+    const approvedAppointments = await appointment.find({ $and: [{ teacherID: teacherData.id}, { status: 'approved' }]})
     res.render('teacherdashboard', {teacher:teacherData, appointments: pendingappointments, approvedappointment: approvedAppointments});
 })
 
@@ -43,14 +45,6 @@ router.get('/request-appointment', (req,res)=>{
     res.render('requestAppointment')
 })
 
-// router.get('/studentsignin', async (req, res) => {
-//     try {
-//       const students = await studentModel.find(); // Fetch all students
-//       res.render('studentdashboard', { students }); // Render studentdashboard.ejs with students data
-//     } catch (error) {
-//         res.status(500).send('Error fetching data');
-//     }
-// });
 
 
 module.exports = router;
